40171aae81162ce98ddf371beee0be12abffbe79
東直希 <naok1207@azumanaokinoMacBook-Pro-2.local>
2024-03-29 11:22:27 +0900

    SlackのURL検証リクエストに対応

--- app/index.js
+++ app/index.js
@@ -2,12 +2,24 @@
 
 // ハンドラ関数の定義
 exports.handler = async (event, context) => {
-  console.log("Event: ", event);
-  // ここに処理ロジックを実装
+  console.log("==================================")
+  console.log(JSON.stringify(event))
+  console.log("==================================")
 
-  // 成功した場合のレスポンス
+  // SlackのURL検証リクエストの場合
+  if (event.type === 'url_verification') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        challenge: event.challenge
+      }),
+    };
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
+    body: JSON.stringify({
+      message: 'Hello from Lambda!',
+    }),
   };
 };
